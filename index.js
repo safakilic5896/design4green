32f1d8aab674cb358777c132af16f1ca45effd8d
skilic1996 <safa.kilic96@gmail.com>
2020-11-05 00:55:42 +0100

    feat(init-http-request)

--- index.js
+++ index.js
@@ -19,8 +19,22 @@ connection.connect(err => {
     }
 })
 
+app.get('/', (req, res) =>{
+    // Website you wish to allow to connect
+
+res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+// Request methods you wish to allow
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+// Request headers you wish to allow
+res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+// Set to true if you need the website to include cookies in the requests sent
+// to the API (e.g. in case you use sessions)
+res.setHeader('Access-Control-Allow-Credentials', true);
+    return res.json({data: 'Hello wordl'})
+})
+
 app.use(cors())
 
-app.listen(4000, () => {
-    console.log('le serveur fonctionne sur le port 4000')
+app.listen(4200, () => {
+    console.log('le serveur fonctionne sur le port 4200')
 })
